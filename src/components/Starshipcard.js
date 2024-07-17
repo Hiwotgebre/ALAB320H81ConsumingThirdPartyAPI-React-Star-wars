b97637195da55a9acfe30d55775f26eb8471d971
Hiwotgebre <hiwotg173@gmail.com>
2024-07-17 12:37:03 -0400

    initial commit

---
+++ src/components/Starshipcard.js
@@ -0,0 +1,11 @@
+function StarshipCard({ starship }) {
+    return (
+      <div>
+        <h3>{starship.name}</h3>
+        {/* Additional details can be added here */}
+      </div>
+    );
+  }
+  
+  export default StarshipCard;
+  
